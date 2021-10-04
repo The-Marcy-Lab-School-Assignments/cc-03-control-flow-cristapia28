bb05c4845c291c85027091112862c50427e73203
cristapia28 <cristiantapianegrete@gmail.com>
2021-10-04 14:35:06 +0000

    Final push

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ function countFromOne(Num){
     for(let i=1; i<=Num; i++1){
         console.log(i)
     }
+
+    
 }
 
 countFromone(88)
@@ -10,7 +12,7 @@ countFromone(88)
 //2
 function countEveryOdd(Num){
     for(let i=1; i<=Num; i++){
-        if(i%2){
+        if(i%2 !==0){
             console.log(i)
         }
         
@@ -20,11 +22,14 @@ function countEveryOdd(Num){
 
 //3
 function isNegative(Num){
+    if(num >= 0){
+        return false
+        
+    }return true
     
-    
-  
+   
 }
-
+console.log(isNegative(-2))
 //4
 function countEveryOdd(){
   
